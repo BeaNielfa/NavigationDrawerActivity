db257cf4ea253eb21debd4404c882862b3577d5a
BeaNielfa <beanielfa1396@gmail.com>
2020-03-30 20:25:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/navigationdraweractivity/AveriasFragment.java
@@ -0,0 +1,31 @@
+package com.example.navigationdraweractivity;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AveriasFragment extends Fragment {
+
+
+    public AveriasFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_averias, container, false);
+    }
+
+}
